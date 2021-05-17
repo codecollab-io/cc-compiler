28126b90b0399df7037be70f20e11326045e835d
Carl Voller <carlvoller8@gmail.com>
2021-05-18 03:10:34 +0800

    Experimental Python REPL

--- setup/tmp/3.9.cpp
+++ setup/tmp/3.9.cpp
@@ -1,6 +1,7 @@
 /**
  *  python3.9.cpp
- *  Modified Python Interpreter to import readline module before any execution.
+ *  Modified Python Interpreter to import readline module before any execution
+ *  and run a custom python REPL.
  *  
  *  Created by: Carl Voller
 */
@@ -14,19 +15,41 @@ inline bool doesFileExist (const std::string& name) {
   return (stat (name.c_str(), &buffer) == 0); 
 }
 
+// Customise the PS1.
+void setPS1() {
+    PyObject *ps1 = PyUnicode_FromString("\u001b[1;3;31m>> \u001b[0m");
+    PySys_SetObject("ps1", ps1);
+    Py_DECREF(ps1);
+}
+
+// Interactive mode.
+void runInteractive() {
+    PyObject *sys, *hook, *result;
+    sys = PyImport_ImportModule("sys");
+
+    if (sys == NULL) { std::cerr << "Unable to initialise sys interactive hook."; return; }
+
+    hook = PyObject_GetAttrString(sys, "__interactivehook__");
+    Py_DECREF(sys);
+
+    if (hook == NULL) {
+        PyErr_Clear();
+        return;
+    }
+
+    result = _PyObject_CallNoArg(hook);
+    Py_DECREF(hook);
+    if (result == NULL) { std::cerr << "Unable to initialise sys interactive hook."; return; }
+    Py_DECREF(result);
+
+    // Show the Python startup version and platform
+    std::cout << "Python " << Py_GetVersion() << " on " << Py_GetPlatform() << "\n";
+
+    setPS1();
+    PyRun_InteractiveLoopFlags(stdin, "<stdin>", NULL);
+}
+
 int main(int argc, char *argv[]) {
-
-    // Checks if a file was even specified
-    if(argv[1] == NULL) {
-        std::cerr << "No file specified." << std::endl;
-        exit(1);
-    }
-
-    // Handles non-existed files instead of showing a Segmentation Fault
-    if(!doesFileExist(argv[1])) {
-        std::cerr << "Can't open file '" << argv[1] << "': No such file or directory" << std::endl;
-        exit(1);
-    }
 
     wchar_t *program = Py_DecodeLocale(argv[0], NULL);
     if (program == NULL) {
@@ -38,15 +61,29 @@ int main(int argc, char *argv[]) {
     Py_SetProgramName(program);
     Py_Initialize();
 
-    PyRun_SimpleString("import readline");
+    // Checks if a file was specified. Run interactive mode otherwise.
+    if(argv[1] == NULL) {
 
-    FILE *file = fopen(argv[1], "r");
+        runInteractive();
+        return 0;
+    }
+
+    // Handles non-existed files instead of showing a Segmentation Fault
+    if(!doesFileExist(argv[1])) {
+        std::cerr << "Can't open file '" << argv[1] << "': No such file or directory" << std::endl;
+        exit(1);
+    }
+
+    // Disables buffering for input statements.
+    PyRun_SimpleString("import readline");
     
-    PyRun_AnyFile(file, argv[1]);
+    // Run the file
+    PyRun_AnyFile(fopen(argv[1], "r"), argv[1]);
     
     if (Py_FinalizeEx() < 0) {
         exit(120);
     }
     PyMem_RawFree(program);
+
     return 0;
 }
